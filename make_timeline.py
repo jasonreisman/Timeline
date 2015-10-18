2fa1cb7defe1387628306cac0ab88f840afe5a13
Jason Reisman <jasonreisman@gmail.com>
2015-10-18 10:50:56 -0400

    gray tickmarks and black labels

--- make_timeline.py
+++ make_timeline.py
@@ -18,13 +18,24 @@ def datetime_from_string(cal, s):
 def estimate_width(callout_size, text_fudge, text):
 	return callout_size[0] + text_fudge[0] + 7.5*len(text)
 
+def add_label(width, height, drawing, date0, date1, dt, label):
+	px = (dt[0] - date0).total_seconds() / (date1 - date0).total_seconds()
+	x = px*width
+	y = height/2
+	dy = 5
+	gray = "rgb(0, 0, 0)"
+	transform = "rotate(180, %i, %i)" % (x, y)
+	drawing.add(drawing.text(label, insert=(x, y-2*dy), stroke='none', fill=gray, font_family="Helevetica", font_size="8pt", text_anchor="end", writing_mode="tb", transform=transform))
+
 def add_tickmark(width, height, drawing, date0, date1, dt, label):
 	px = (dt[0] - date0).total_seconds() / (date1 - date0).total_seconds()
 	x = px*width
 	y = height/2
-	drawing.add(drawing.line((x,y-10), (x,y+10), stroke="black", stroke_width=2))
+	gray = "rgb(192, 192, 192)"
+	dy = 5
+	drawing.add(drawing.line((x,y-dy), (x,y+dy), stroke='black', stroke_width=2))
 	transform = "rotate(180, %i, %i)" % (x, y)
-	drawing.add(drawing.text(label, insert=(x, y-15), font_family="Helevetica", font_size="8pt", text_anchor="end", writing_mode="tb", transform=transform))
+	drawing.add(drawing.text(label, insert=(x, y-2*dy), stroke='none', fill=gray, font_family="Helevetica", font_size="8pt", text_anchor="end", writing_mode="tb", transform=transform))
 
 def create_timeline(filename):
 	s = ''
@@ -43,8 +54,15 @@ def create_timeline(filename):
 	height = data['height']
 	drawing = svgwrite.Drawing('out.svg', size=(width, height))
 	drawing.add(drawing.line((0, height/2), (width, height/2), stroke="black", stroke_width=3))
+	# add tickmarks
 	add_tickmark(width, height, drawing, date0, date1, start_date, str(start_date[0]))
 	add_tickmark(width, height, drawing, date0, date1, end_date, str(end_date[0]))
+	if 'num_ticks' in data:
+		num_ticks = data['num_ticks']
+		for j in range(1, num_ticks):
+			tick_delta = datetime.timedelta(seconds=j*delta.total_seconds()/num_ticks)
+			tickmark_date = start_date[0] + tick_delta
+			add_tickmark(width, height, drawing, date0, date1, [tickmark_date], str(tickmark_date))		
 	callouts = data['callouts']
 	# sort callouts
 	sorted_dates = []
@@ -59,7 +77,7 @@ def create_timeline(filename):
 	for event_date in sorted_dates:
 		event = inv_callouts[event_date]
 		#event_date = datetime_from_string(cal, date_string)
-		num_sec = (event_date[0] - start_date[0]).total_seconds()
+		num_sec = (event_date[0] - date0).total_seconds()
 		percent_width = num_sec/total_secs
 		if percent_width < 0 or percent_width > 1:
 			continue
@@ -81,6 +99,7 @@ def create_timeline(filename):
 		drawing.add(drawing.line((x, height/2), (x, y), stroke='black', stroke_width=2))
 		drawing.add(drawing.line((x+1, y), (x - callout_size[0], y), stroke='black', stroke_width=2))		
 		drawing.add(drawing.text(event, insert=(x - callout_size[0] - text_fudge[0], y + text_fudge[1]), font_family="Helevetica", font_size="10pt", text_anchor="end"))
+		add_label(width, height, drawing, date0, date1, event_date, str(event_date[0]))
 		drawing.add(drawing.circle((x, height/2), r=5, stroke='black', stroke_width=2, fill='white'))
 		prev_x.append(x)
 		prev_level.append(k)
